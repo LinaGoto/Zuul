eb39d997cf008d4e936da39d442244305a709546
Lina Goto <452215@bsd48.org>
2023-11-24 13:05:16 -0800

    add

--- zuul.cpp
+++ zuul.cpp
@@ -21,4 +21,31 @@ struct positions_s{
 }
 
 int main(){
+  char inventory [10];
+  char input [10];
+  book play = true;
+
+  do{
+    cout << "INVENTORY/MOVE/PICKUP/DROP" << endl;
+
+    cin.get(input, 0);
+    cin.get();
+
+    //inventory
+    if (input[0] == 'I' || input[0] == 'i'){
+      cout << "you have this in your inventory:" << endl;
+    }
+    //move
+    if (input[0] == 'M' || input[0] == 'm'){
+      cout << "you could move" << endl;
+    }
+    //pick up
+    if (input[0] == 'P' || input[0] == 'p'){
+      cout << "you could pick up" << endl;
+    }
+    //inventory
+    if (input[0] == 'D' || input[0] == 'd'){
+      cout << "you could drop" << endl;
+    }    
+
 }
