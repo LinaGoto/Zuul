a550f8e4b23069d3d51508d3026d29e919f2a0cd
Lina Goto <452215@bsd48.org>
2023-11-24 12:57:59 -0800

    add

--- zuul.cpp
+++ zuul.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 #include <cstring>
+
+using namespace std;
+
+struct room_s{
+  bool exist;
+  int item;
+}
+
+static char *itemlist[]= {
+  "circle",
+  "triangle",
+  "star",
+  "heart",
+}
+
+struct positions_s{
+  int x,y
+  int num_items;
+}
+
+int main(){
+}
