dc69b4d96da1c314182a02f7a0a7d48e33fc9e66
Lina Goto <452215@bsd48.org>
2023-11-26 16:44:18 -0800

    finish project

--- zuul.cpp
+++ zuul.cpp
@@ -3,16 +3,17 @@
 
 using namespace std;
 
-
-//room set up
+// room structure set up
 struct room_s{
   int exist;
   int item;
   int goal;
 };
 
-//items dropped
-static const char *itemlist[]= {
+#define	MAX_ITEM  5
+
+// drop items
+static const char *itemlist[MAX_ITEM]= {
   "circle",
   "triangle",
   "star",
@@ -20,87 +21,85 @@ static const char *itemlist[]= {
   "heart",
 };
 
-//position and items in inventory
+// my position and inventory items
 struct position_s{
   int x,y;
   int num_item;
-  int item[6];
+  int item[MAX_ITEM + 1];
 };
 
-int main(){
+int main(void) {
   char input [10];
   bool play = true;
   struct room_s room[8][5];
   struct position_s mypos;
-  int count = 0;
-
-  //initialize - clear the board
+  
+  // initialize - clear the board
   for(int i=0; i < 8; i++){
     for (int j=0; j < 5; j++){
-      room[i][j].exist = 0;
+      room[i][j].exist =  0;
       room[i][j].item  = -1;
-      room[i][j].goal = 0;
+      room[i][j].goal  =  0;
     }
   }
 
-  //making existing rooms
+  // making existing rooms
   for(int i = 1; i < 6; i++){
     for (int j = 1; j < 4; j++){
       room[i][j].exist = 1;
-      
     }
   }
 
-  //setup the goal and where the items are placed
+  // setup the goal
   room[6][2].exist = 1;
-  room[6][2].goal = 1;
-  room[1][1].item = 0;
-  room[5][1].item = 1;
-  room[3][2].item = 2;
-  room[2][3].item = 3;
-  room[4][5].item = 4;
+  room[6][2].goal  = 1;
+  
+  // placing items
+  room[1][1].item = 0; // circle
+  room[5][1].item = 1; // triangle
+  room[3][2].item = 2; // star
+  room[2][3].item = 3; // square
+  room[4][3].item = 4; // heart
 
-  //starting position
+  // my initial position
   mypos.x = 6;
   mypos.y = 2;
   mypos.num_item = 0;
-
-  //empty the inventory
-  for (int i=0; i<7; i++){
+  // empty the inventory
+  for (int i=0; i<MAX_ITEM + 1; i++){
     mypos.item[i] = -1;
   }
-
   
+  // main loop
   do{
     //if all conditions for winning is achived - break to win
     if ((room[mypos.x][mypos.y].goal == 1) & (mypos.num_item == 5)) break;
     //print current place
-    cout << "you are in room " << mypos.x << "-" << mypos.y << endl;
+    cout << "You are in room " << mypos.x << "-" << mypos.y << "." << endl;
     //print what is in the room
     if (room[mypos.x][mypos.y].item >= 0){
-      cout << "there is an " << itemlist[room[mypos.x][mypos.y].item]
-	   << " on the floor" << endl;
-    }
-    if (room[mypos.x][mypos.y].item < 0){
-      cout << "there is no item to be picked up" << endl;
+      cout << "There is an " << itemlist[room[mypos.x][mypos.y].item]
+	   << " on the floor." << endl;
+    } else {
+      cout << "There is no item to be picked up." << endl;
     }
     //print possible exists
-    cout <<  "There are exits: ";
+    cout <<  "There are exits:";
     if(room[mypos.x + 1][mypos.y].exist == 1){
-	cout << "EAST ";
+	cout << " EAST";
       }
-    if(room[mypos.x -1][mypos.y].exist == 1){
-	cout << "WEST ";
+    if(room[mypos.x - 1][mypos.y].exist == 1){
+	cout << " WEST";
       }
     if(room[mypos.x][mypos.y - 1].exist == 1){
-	cout << "NORTH ";
+	cout << " NORTH";
       }
     if(room[mypos.x][mypos.y + 1].exist == 1){
-	cout << "SOUTH ";
+	cout << " SOUTH";
       }
     cout << endl;
     //ask for move
-    cout << "What would you like to do ? enter: MOVE/PICKUP/DROP" << endl;
+    cout << "What would you like to do? Enter: [M]ove / [P]ickup / [D]rop" << endl;
     cin.get(input, 10);
     cin.get();
     //move
@@ -113,98 +112,95 @@ int main(){
       if (move[0] == 'E' || move[0] == 'e'){
 	if(room[mypos.x + 1][mypos.y].exist == 1){
 	  mypos.x ++;
-	}
-	else{
-	  cout << "you cannot go east" << endl;
+	} else {
+	  cout << "You cannot go east." << endl;
 	}
       }
       //west
       if (move[0] == 'W' || move[0] == 'w'){
 	if(room[mypos.x - 1][mypos.y].exist == 1){
 	  mypos.x --;
-	}
-	else{
-	  cout << "you cannot go west" << endl;
+	} else {
+	  cout << "You cannot go west." << endl;
 	}
       }
       //north
       if (move[0] == 'N' || move[0] == 'n'){
 	if(room[mypos.x][mypos.y - 1].exist == 1){
 	  mypos.y --;
-	}
-	else{
-	  cout << "you cannot go north" << endl;
+	} else {
+	  cout << "You cannot go north." << endl;
 	}
       }
       //south
       if (move[0] == 'S' || move[0] == 's'){
 	if(room[mypos.x][mypos.y + 1].exist == 1){
 	  mypos.y ++;
-	}
-	else{
-	  cout << "you cannot go south" << endl;
+	} else {
+	  cout << "You cannot go south." << endl;
 	}
       }
-      
     }
     
-    //pick up
+    // pick up
     if (input[0] == 'P' || input[0] == 'p'){
-      //if there is an item to pick up
+      // if there is an item to pick up
       if (room[mypos.x][mypos.y].item >= 0){
-	//add to mypos items
-	mypos.item[count] = room[mypos.x][mypos.y].item;
-	count ++;
-	//take out from room
-       room[mypos.x][mypos.y].item = -1;
-	cout << "you had successfully picked up the item and had stored it into your inventory" << endl;   
-      }
+	// find empty location
+	int i = 0;
+	while (mypos.item[i] >= 0) i ++;
+	// add item to mypos
+	mypos.item[i] = room[mypos.x][mypos.y].item;
 
-      /*change so items could be placed in any slot thats opened*/
-      
-      else{
-	cout << "there is no item to be picked up" << endl;
+	// take out from room
+	room[mypos.x][mypos.y].item = -1;
+        // increment number of items that I have
+	mypos.num_item ++;
+	cout << "You had successfully picked up the item and had stored it into your inventory." << endl;   
+      } else{
+	cout << "There is no item to be picked up." << endl;
       }
-      
     }
-
     
-    //drop
+    // drop
     if(input[0] == 'D' || input[0] == 'd'){
-      cout << "what would you like to drop(enter number): ";
-      int counting = 0;
-      //list out all numbers listed in inventory
-      for(int i=0; i<7; i++){
-	if(mypos.item[i] != -1){
-	  cout << mypos.item[i] << "."<< itemlist[mypos.item[i]] << " ";
-	  counting ++;
+      // if there was nothing printed out, there is nothing in inventory
+      if (mypos.num_item <= 0){
+	cout << "There is nothing in your inventory that you could drop." << endl;
+      } else {
+	cout << "What would you like to drop? ";
+	// list out all numbers listed in the inventory
+	for(int i=0; i<MAX_ITEM; i++){
+	  if(mypos.item[i] >= 0){
+	    cout << mypos.item[i] << ". " << itemlist[mypos.item[i]] << " ";
+	  }
 	}
-      }
-      //if there was nothing printed out, there is nothing in inventory
-      if (counting == 0){
-	cout << "there is nothing in your inventory that you could drop" << endl;
-      }
-      //if something is listed
-      if (counting > 0){
-	//input what to drop
 	cout << endl;
+
+	cout << "Enter number : ";
+	// input what to drop
 	int dropping = 0;
 	cin >> dropping;
 	cin.get();
-	
-	for (int i=0; i<7; i++){
-	  //see if there is the item inputed existing in inventory
-	  if (mypos.item[i] == mypos.item[dropping]){
-	    //put the number in the room
+
+	for (int i=0; i<MAX_ITEM; i++){
+	  // see if there is the item inputed existing in inventory
+	  if (mypos.item[i] == dropping){
+	    // put the number in the room
 	    room[mypos.x][mypos.y].item = mypos.item[i];
-	    //clear number form inventory
-	    mypos.item[i] == -1;
-	    //when the number does not exist
+	    // clear number form inventory
+	    mypos.item[i] = -1;
+	    // decrement number of items that I have
+	    mypos.num_item --;
 	  }
 	}
-	
       }
     }
     
   } while (play == true);
+
+  // Goal
+  cout << endl << "Congratulations! You have successfully cleared this maze." << endl;
+
+  return 0;
 }
